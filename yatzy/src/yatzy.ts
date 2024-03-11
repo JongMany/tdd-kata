444101c9a60e3a6d409e3c9d4c19ef7cf34209c2
JongMany <blackberry1114@naver.com>
2024-03-11 20:47:34 +0900

    [ :sparkles: || Feat ] yatzy tdd

---
+++ yatzy/src/yatzy.ts
@@ -0,0 +1,154 @@
+type DiceNum = 1 | 2 | 3 | 4 | 5 | 6;
+
+type DiceCountMapper = {
+  [key in DiceNum]: number;
+};
+
+export default class Yatzy {
+  private readonly dice: number[];
+  private readonly diceCountMapper: DiceCountMapper;
+  private yatzy = false;
+  constructor(d1: number, d2: number, d3: number, d4: number, d5: number) {
+    this.dice = [d1, d2, d3, d4, d5];
+    this.diceCountMapper = this.dice.reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur]: (acc[cur as DiceNum] || 0) + 1,
+      }),
+      {} as DiceCountMapper
+    );
+  }
+
+  get yatzee() {
+    return this.yatzy;
+  }
+
+  // upper section
+  aces(): number {
+    return this.singles(1);
+  }
+  twos(): number {
+    return this.singles(2);
+  }
+  threes(): number {
+    return this.singles(3);
+  }
+  fours(): number {
+    return this.singles(4);
+  }
+  fives(): number {
+    return this.singles(5);
+  }
+  sixes(): number {
+    return this.singles(6);
+  }
+
+  // lower section
+  chance(): number {
+    return this.dice.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  ThreeOfAKind(): number {
+    if (this.checkQuantity(3)) {
+      return this.dice.reduce((acc, curr) => acc + curr, 0);
+    } else {
+      throw new Error("No three of a kind");
+    }
+  }
+
+  FourOfAKind(): number {
+    if (this.checkQuantity(4)) {
+      return this.dice.reduce((acc, curr) => acc + curr, 0);
+    } else {
+      throw new Error("No four of a kind");
+    }
+  }
+
+  FullHouse(): number {
+    if (this.checkQuantity(3) && this.checkQuantity(2)) {
+      return this.dice.reduce((acc, curr) => acc + curr, 0);
+    } else {
+      throw new Error("No full house");
+    }
+  }
+
+  yahtzee(): number {
+    if (this.checkQuantity(5)) {
+      this.yatzy = true;
+      return 50;
+    } else {
+      throw new Error("No yahtzee");
+    }
+  }
+
+  // 4개의 눈이 연속일 때 30점
+  smallStraight(): number {
+    if (this.checkDiceSequence(4)) {
+      return 30;
+    } else {
+      throw new Error("No small straight");
+    }
+  }
+
+  // 5개의 눈이 연속일 때 40점
+  largeStraight(): number {
+    if (this.checkDiceSequence(5)) {
+      return 40;
+    } else {
+      throw new Error("No large straight");
+    }
+  }
+
+  /**
+   * @description
+   * 한 개의 값만을 필터링해서 더해주는 메서드
+   * */
+  private singles(n: number): number {
+    return this.dice
+      .filter((d) => d === n)
+      .reduce((acc, curr) => acc + curr, 0);
+  }
+
+  /**
+   * @description
+   * 원하는 중복 개수를 체크하는 함수
+   */
+  private checkQuantity(count: DiceNum) {
+    return Object.values(this.diceCountMapper).some((v) => v === count);
+  }
+
+  /**
+   * @description
+   * 눈이 연속인지 체크하는 함수
+   *  */
+  private checkDiceSequence(seqCount = 4): boolean {
+    // const sortedDice = this.dice.sort((a, b) => a - b);
+    const initialValue: number[] = Array(6).fill(0);
+    const bucket = this.dice.reduce((acc, cur) => {
+      acc[cur - 1] = (acc[cur - 1] || 0) + 1;
+      return acc;
+    }, initialValue);
+
+    let seq = 0;
+    let tempSeq = 0;
+    for (const cnt of bucket) {
+      if (cnt > 0) {
+        seq++;
+        if (seq === seqCount) {
+          return true;
+        }
+      } else {
+        tempSeq = seq;
+        seq = 0;
+      }
+    }
+
+    if (seq === seqCount) {
+      return true;
+    } else if (tempSeq === seqCount) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ yatzy/tests/yatzy.spec.ts
@@ -0,0 +1,138 @@
+import Yatzy from "../src/yatzy";
+
+describe("chances", () => {
+  it("chance 메서드는 모든 dice 눈의 합을 넘긴 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 3, 4, 5);
+    const yatzy2 = new Yatzy(1, 3, 3, 4, 5);
+
+    expect(yatzy1.chance()).toBe(15);
+    expect(yatzy2.chance()).toBe(16);
+  });
+});
+
+describe("Yatzy's Upper section", () => {
+  it("ones 메서드는 1을 필터링해서 더한 값을 반환합니다..", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(1, 2, 1, 4, 1);
+    expect(yatzy1.aces()).toBe(2);
+    expect(yatzy2.aces()).toBe(3);
+  });
+
+  it("twos 메서드는 2를 필터링해서 더한 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(1, 2, 2, 4, 2);
+    expect(yatzy1.twos()).toBe(2);
+    expect(yatzy2.twos()).toBe(6);
+  });
+
+  it("threes 메서드는 3을 필터링해서 더한 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(1, 3, 3, 3, 3);
+    expect(yatzy1.threes()).toBe(0);
+    expect(yatzy2.threes()).toBe(12);
+  });
+
+  it("fours 메서드는 4를 필터링해서 더한 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(4, 4, 4, 5, 5);
+    expect(yatzy1.fours()).toBe(4);
+    expect(yatzy2.fours()).toBe(12);
+  });
+
+  it("fives 메서드는 5를 필터링해서 더한 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(4, 4, 5, 5, 5);
+    expect(yatzy1.fives()).toBe(5);
+    expect(yatzy2.fives()).toBe(15);
+  });
+
+  it("sixes 메서드는 6을 필터링해서 더한 값을 반환합니다.", () => {
+    const yatzy1 = new Yatzy(1, 2, 1, 4, 5);
+    const yatzy2 = new Yatzy(4, 4, 6, 5, 5);
+    expect(yatzy1.sixes()).toBe(0);
+    expect(yatzy2.sixes()).toBe(6);
+  });
+});
+
+describe("Yatzy's Lower section", () => {
+  it("ThreeOfAKind 메서드는 3개의 눈이 동일할 때, 주사위 5개의 합을 반환합니다..", () => {
+    const yatzy = new Yatzy(3, 3, 3, 4, 4);
+
+    expect(yatzy.ThreeOfAKind()).toBe(17);
+  });
+
+  it("ThreeOfAKind 메서드는 3개의 눈이 동일하지 않는 경우, 에러를 반환합니다...", () => {
+    const yatzy = new Yatzy(3, 3, 3, 3, 4);
+
+    expect(() => yatzy.ThreeOfAKind()).toThrow("No three of a kind");
+  });
+
+  it("FourOfAKind 메서드는 4개의 눈이 동일할 때, 주사위 5개의 합을 반환합니다..", () => {
+    const yatzy = new Yatzy(3, 3, 3, 3, 4);
+
+    expect(yatzy.FourOfAKind()).toBe(16);
+  });
+
+  it("FourOfAKind 메서드는 4개의 눈이 동일하지 않는 경우, 에러를 반환합니다...", () => {
+    const yatzy = new Yatzy(3, 3, 3, 3, 3);
+
+    expect(() => yatzy.FourOfAKind()).toThrow("No four of a kind");
+  });
+
+  it("FullHouse 메서드는 3개의 눈이 동일하고, 2개의 눈이 동일할 때, 주사위 5개의 합을 반환합니다..", () => {
+    const yatzy = new Yatzy(3, 3, 3, 4, 4);
+
+    expect(yatzy.FullHouse()).toBe(17);
+  });
+
+  it("FullHouse 메서드는 3개의 눈이 동일하지 않거나, 2개의 눈이 동일하지 않는 경우, 에러를 반환합니다...", () => {
+    const yatzy = new Yatzy(3, 3, 3, 4, 5);
+
+    expect(() => yatzy.FullHouse()).toThrow("No full house");
+  });
+
+  it("Yahtzee 메서드는 5개의 눈이 동일할 때, 50을 반환하고, yatzee 값을 true로 바꿉니다.", () => {
+    const yatzy = new Yatzy(3, 3, 3, 3, 3);
+
+    expect(yatzy.yahtzee()).toBe(50);
+    expect(yatzy.yatzee).toBe(true);
+  });
+
+  it("Yahtzee 메서드는 5개의 눈이 동일하지 않을 때, 에러를 반환합니다...", () => {
+    const yatzy = new Yatzy(3, 3, 3, 3, 4);
+
+    expect(() => yatzy.yahtzee()).toThrow("No yahtzee");
+  });
+
+  it("smallStraight 메서드는 4개 이상의 수가 연속일 때, 30점을 반환합니다...", () => {
+    const yatzy1 = new Yatzy(1, 2, 3, 4, 6);
+    const yatzy2 = new Yatzy(1, 2, 3, 4, 5);
+
+    expect(yatzy1.smallStraight()).toBe(30);
+    expect(yatzy2.smallStraight()).toBe(30);
+  });
+
+  it("smallStraight 메서드는 4개 이상의 수가 연속이 아닐 때, 에러를 반환합니다...", () => {
+    const yatzy1 = new Yatzy(1, 2, 3, 5, 6);
+    const yatzy2 = new Yatzy(1, 2, 4, 5, 6);
+
+    expect(() => yatzy1.smallStraight()).toThrow("No small straight");
+    expect(() => yatzy2.smallStraight()).toThrow("No small straight");
+  });
+
+  it("largeStraight 메서드는 5개의 수가 연속일 때, 40점을 반환합니다...", () => {
+    const yatzy1 = new Yatzy(1, 2, 3, 4, 5);
+    const yatzy2 = new Yatzy(3, 2, 4, 5, 6);
+
+    expect(yatzy1.largeStraight()).toBe(40);
+    expect(yatzy2.largeStraight()).toBe(40);
+  });
+
+  it("largeStraight 메서드는 5개의 수가 연속이 아닐 때, 에러를 반환합니다...", () => {
+    const yatzy1 = new Yatzy(1, 2, 3, 4, 6);
+    const yatzy2 = new Yatzy(2, 2, 4, 5, 6);
+
+    expect(() => yatzy1.largeStraight()).toThrow("No large straight");
+    expect(() => yatzy2.largeStraight()).toThrow("No large straight");
+  });
+});
